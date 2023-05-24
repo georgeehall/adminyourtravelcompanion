a1fb6d198727feac735bef5b33c42bd3a546ea20
George Hall <georgeehall@hotmail.co.uk>
2023-05-24 15:22:21 +0100

    Continuing with layout

--- welcome.php
+++ welcome.php
@@ -8,9 +8,10 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     exit;
 }
 ?>
- 
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Welcome</title>
@@ -33,108 +34,180 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     }
     </style>
 </head>
+
 <body>
 
 
-<main>
+    <main>
 
-  <div class="flex-shrink-0 p-3 bg-white" style="width: 280px;">
-    <a href="/" class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
-      <svg class="bi me-2" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
-      <span class="fs-5 fw-semibold">Fly Frew CMS</span>
-    </a>
-    <ul class="list-unstyled ps-0">
-      <!-- <li class="mb-1">
-        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
-          Quick Access
-        </button>
-        <div class="collapse show" id="home-collapse">
-          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="#" class="link-dark rounded">Overview</a></li>
-            <li><a href="#" class="link-dark rounded">Updates</a></li>
-            <li><a href="#" class="link-dark rounded">Reports</a></li>
-          </ul>
-        </div>
-      </li> -->
-      <li class="mb-1">
-        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
-          Create quotes
-        </button>
-        <div class="collapse" id="dashboard-collapse">
-          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="#" class="link-dark rounded">Hotel Only</a></li>
-            <li><a href="#" class="link-dark rounded">Flight Only</a></li>
-            <li><a href="#" class="link-dark rounded">Hotel Only</a></li>
-          </ul>
-        </div>
-      </li>  
-      <li class="mb-1">
-        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse1" aria-expanded="false">
-          View Quotes
-        </button>
-        <div class="collapse" id="orders-collapse1">
-          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="#" class="link-dark rounded">Overview</a></li>
-            <li><a href="#" class="link-dark rounded">Create Content</a></li>
-          </ul>
-        </div>
-      </li>
-      <li class="mb-1">
-        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
-          Create a booking
-        </button>
-        <div class="collapse" id="orders-collapse">
-          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="#" class="link-dark rounded">Overview</a></li>
-            <li><a href="#" class="link-dark rounded">Create Content</a></li>
-          </ul>
-        </div>
-      </li>
+        <div class="flex-shrink-0 p-3 bg-white" style="width: 280px;">
+            <a href="/" class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
+                <svg class="bi me-2" width="30" height="24">
+                    <use xlink:href="#bootstrap" />
+                </svg>
+                <span class="fs-5 fw-semibold" style="
+    margin-left: -25px;
+">Your Travel Companion</span>
+            </a>
+            <ul class="list-unstyled ps-0">
+                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                    <li><a href="#" class="link-dark rounded">Dashboard</a></li>
 
-      <li class="mb-1">
-        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse2" aria-expanded="false">
-          View Bookings
-        </button>
-        <div class="collapse" id="orders-collapse2">
-          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="#" class="link-dark rounded">Overview</a></li>
-            <li><a href="#" class="link-dark rounded">Create Content</a></li>
-          </ul>
-        </div>
-      </li>
+                </ul>
+                <li class="mb-1">
+                    <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse"
+                        data-bs-target="#dashboard-collapse" aria-expanded="false">
+                        Create quotes
+                    </button>
+                    <div class="collapse" id="dashboard-collapse">
+                        <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                            <li><a href="#" class="link-dark rounded">Overview</a></li>
+                            <li><a href="#" class="link-dark rounded">Hotel Only</a></li>
+                            <li><a href="#" class="link-dark rounded">Flight Only</a></li>
+                            <li><a href="#" class="link-dark rounded">Packages</a></li>
+                            <li><a href="#" class="link-dark rounded">Other</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="mb-1">
+                    <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse"
+                        data-bs-target="#orders-collapse1" aria-expanded="false">
+                        View Quotes
+                    </button>
+                    <div class="collapse" id="orders-collapse1">
+                        <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
 
-      <li class="border-top my-3"></li>
-      <li class="mb-1">
-        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
-          My Account
-        </button>
-        <div class="collapse" id="account-collapse">
-          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-            <li><a href="reset-password.php" class="link-dark rounded">Reset Password</a></li>
-            <li><a href="logout.php" class="link-dark rounded">Sign out</a></li>
-          </ul>
+                            <li><a href="#" class="link-dark rounded">Hotel Only</a></li>
+                            <li><a href="#" class="link-dark rounded">Flight Only</a></li>
+                            <li><a href="#" class="link-dark rounded">Packages</a></li>
+                            <li><a href="#" class="link-dark rounded">Other</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="mb-1">
+                    <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse"
+                        data-bs-target="#orders-collapse" aria-expanded="false">
+                        Create a booking
+                    </button>
+                    <div class="collapse" id="orders-collapse">
+                        <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                            <li><a href="#" class="link-dark rounded">Hotel Only</a></li>
+                            <li><a href="#" class="link-dark rounded">Flight Only</a></li>
+                            <li><a href="#" class="link-dark rounded">Packages</a></li>
+                            <li><a href="#" class="link-dark rounded">Other</a></li>
+                        </ul>
+                    </div>
+                </li>
+
+                <li class="mb-1">
+                    <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse"
+                        data-bs-target="#orders-collapse2" aria-expanded="false">
+                        View Bookings
+                    </button>
+                    <div class="collapse" id="orders-collapse2">
+                        <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                            <li><a href="#" class="link-dark rounded">Overview</a></li>
+                            <li><a href="#" class="link-dark rounded">Hotel Only</a></li>
+                            <li><a href="#" class="link-dark rounded">Flight Only</a></li>
+                            <li><a href="#" class="link-dark rounded">Packages</a></li>
+                            <li><a href="#" class="link-dark rounded">Other</a></li>
+                        </ul>
+                    </div>
+                </li>
+
+                <li class="border-top my-3"></li>
+                <li class="mb-1">
+                    <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse"
+                        data-bs-target="#account-collapse3" aria-expanded="false">
+                        Destination Info
+                    </button>
+                    <div class="collapse" id="account-collapse3">
+                        <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                            <li><a href="reset-password.php" class="link-dark rounded">Mexico</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="border-top my-3"></li>
+                <li class="mb-1">
+                    <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse"
+                        data-bs-target="#account-collapse4" aria-expanded="false">
+                        Airline Info
+                    </button>
+                    <div class="collapse" id="account-collapse4">
+                        <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                            <li><a href="reset-password.php" class="link-dark rounded">British Airways</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="border-top my-3"></li>
+                <li class="mb-1">
+                    <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse"
+                        data-bs-target="#account-collapse" aria-expanded="false">
+                        My Account
+                    </button>
+                    <div class="collapse" id="account-collapse">
+                        <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                            <li><a href="reset-password.php" class="link-dark rounded">Reset Password</a></li>
+                            <li><a href="logout.php" class="link-dark rounded">Sign out</a></li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
         </div>
-      </li>
-    </ul>
+
+        <div class="b-example-divider"></div>
+
+        <div class="container" style="padding-top: 20px;">
+
+        <div class="row">
+          <div class="col-md-4">
+
+            <div class="card text-center">
+                <div class="card-header">Total Bookings</div>
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
   </div>
+  <div class="col-md-4">
 
-  <div class="b-example-divider"></div>
+<div class="card text-center">
+    <div class="card-header">Total Revenue</div>
+    <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
+</div>
+<div class="col-md-4">
+
+<div class="card text-center">
+    <div class="card-header">Total Commission</div>
+    <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
+</div>
 
 
-<div class="container" style="padding-top: 20px;">
-<div class="col-md-6">
-            <h1 class="mb-3">Flight Quotes</h1>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Departure Date</th>
-                        <th>Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
+            <div class="col-md-6">
+                <h1 class="mb-3">Flight Quotes</h1>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Departure Date</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
 			// Connect to the database
             $conn = mysqli_connect("localhost", "root", "", "yourtravelcompanion");
 			// Check connection
@@ -169,22 +242,22 @@ while ($row = mysqli_fetch_assoc($result)) {
 			// Close the database connection
 			mysqli_close($conn);
 			?>
-                </tbody>
-            </table>
-        </div>
-        <div class="col-md-6">
-            <h1 class="mb-3">Hotel Quotes</h1>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Check In Date</th>
-                        <th>Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-md-6">
+                <h1 class="mb-3">Hotel Quotes</h1>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Check In Date</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
 			// Connect to the database
             $conn = mysqli_connect("localhost", "root", "", "yourtravelcompanion");
 			// Check connection
@@ -219,23 +292,23 @@ while ($row = mysqli_fetch_assoc($result)) {
 			// Close the database connection
 			mysqli_close($conn);
 			?>
-                </tbody>
-            </table>
-        </div>
+                    </tbody>
+                </table>
+            </div>
 
-        <div class="col-md-12">
-        <h1 class="mb-3">Package Quotes</h1>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Outbound Date</th>
-                        <th>Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
+            <div class="col-md-12">
+                <h1 class="mb-3">Package Quotes</h1>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Outbound Date</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
 			// Connect to the database
             $conn = mysqli_connect("localhost", "root", "", "yourtravelcompanion");
 			// Check connection
@@ -270,16 +343,19 @@ while ($row = mysqli_fetch_assoc($result)) {
 			// Close the database connection
 			mysqli_close($conn);
 			?>
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
-</div>
+        </div>
 
-</main>
+    </main>
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
     <script src="sidebars.js"></script>
 </body>
+
 </html>
