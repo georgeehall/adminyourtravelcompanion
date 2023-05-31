f4c01cfbfd67d8e97eb497ffd027b6b40ee93ff7
George Hall <georgeehall@hotmail.co.uk>
2023-05-31 15:20:12 +0100

    Continuing with layout

--- index.php
+++ index.php
@@ -2,14 +2,16 @@
 // Initialize the session
 session_start();
  
+// Include config file
+require_once "config.php";
+
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     header("location: welcome.php");
     exit;
 }
  
-// Include config file
-require_once "config.php";
+
  
 // Define variables and initialize with empty values
 $username = $password = "";
